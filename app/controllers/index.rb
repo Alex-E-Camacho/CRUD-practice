7b45f80014f89656dade59040c9f52d832339f07
Alex Camacho <email": null>
2017-05-22 12:12:06 -0400

    Add login/logout routes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,23 @@
 get '/' do
   erb :index
 end
+
+get '/login' do
+  erb :login
+end
+
+post '/login' do
+  @user = User.find_by(email: params["email"])
+  if @user && @user.authenticate(params["password"])
+    session[:user_id] = @user.id
+    redirect "/users/#{@user.id}"
+  else
+    @errors = ["Email and/or password incorrect"]
+    erb :login
+  end
+end
+
+get '/logout' do
+  session.clear
+  redirect '/'
+end
