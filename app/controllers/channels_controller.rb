ab6c69284ea40c9e0715f0e4c48fd021fdedfd3d
Alex Camacho <email": null>
2017-05-22 16:40:42 -0400

    Create both /channels and individual channel show page

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -2,3 +2,8 @@ get '/channels' do
   @channels = Channel.all
   erb :'/channels/index'
 end
+
+get '/channels/:channel_id' do
+  @channel = Channel.find_by(id: params[:channel_id])
+  erb :'/channels/show'
+end
