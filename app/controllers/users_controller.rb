c458c39ac2b75200450aa4761a94d368ab39d277
Alex Camacho <email": null>
2017-05-22 11:44:21 -0400

    Add users/new route, post /users route, and /users/:id show route

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+get '/users' do
+  "This is the /users get route"
+end
+
+get '/users/new' do
+  erb :'/users/new'
+end
+
+post '/users' do
+  @user = User.new(params["user"])
+  if @user.save
+    redirect "/users/#{@user.id}"
+  else
+    @errors = @user.error.full_messages
+    erb :'/users/new'
+  end
+end
+
+get '/users/:id' do
+  @user = User.find_by(id: params[:id])
+  erb :'/users/show'
+end
